cee2be2e7bdc4e7a3ed95c668c96fe507dce0818
Sam Dieck <samuel.dieck@gmail.com>
2017-02-01 21:06:28 -0800

    Algorithms checkpoint 6 completed

---
+++ 02-algorithms/06-algorithms-complexity-improvement/improving_complexity_version_three.rb
@@ -0,0 +1,5 @@
+class SpaceCompexity
+  def time_complexity_resolution(*arrays)
+    combined_array = arrays.flatten.sort()
+  end
+end
---
+++ 02-algorithms/06-algorithms-complexity-improvement/improving_complexity_version_two.rb
@@ -0,0 +1,12 @@
+class TimeCompexity
+  def space_complexity_resolution(*arrays)
+    combined_array = arrays.flatten!
+    array_to_hash = {}
+
+    combined_array.each_with_index do |index, value|
+      array_to_hash[index] = value
+    end
+    # Sorting isn't necessary at this point
+    array_to_hash
+  end
+end
